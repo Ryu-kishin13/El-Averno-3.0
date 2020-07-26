fdf82d26b14a630a6a5d66fe7a3c744171924dbe
locomyke666@gmail.com <Ryu-kishin13@users.noreply.github.com>
2020-07-26 11:36:17 -0400

    .

--- Asignaciones/Asignacion10/js/login.js
+++ Asignaciones/Asignacion10/js/login.js
@@ -1,27 +1,11 @@
-$(document).ready(function(){
-    $("#inicio").click(function(){
 
-        var user = $("#user").val();
-        var pass = $("#pass").val();
+$('.toggle').click(function(){
+    $('.formulario').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+    }, "slow");
+});
 
-        if(user == "")
-        {
-            $("#mensaje_usuario").fadeIn("");
-            return false;
-        }
-        //en otro caso, el mensaje no se muestra
-        else
-        {
-            $("#mensaje_usuario").fadeOut();
-            if(pass == "")
-            {
-                $("mensaje_contraseña").fadeIn("");
-                return false;
-            }
-            else
-            {
-                $("mensaje_contrseña").fadeOut();
-            }
-        }
-    })
-})
+
