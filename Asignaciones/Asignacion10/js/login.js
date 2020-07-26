2a0dc145caeb86c8c799e2b03beeef49b28ae435
locomyke666@gmail.com <Ryu-kishin13@users.noreply.github.com>
2020-07-25 22:44:03 -0400

    .

--- Asignaciones/Asignacion10/js/login.js
+++ Asignaciones/Asignacion10/js/login.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
     $("#inicio").click(function(){
         var usuario= $("#user").val();
         var contraseña = $("#pass").val();
+
+        if(usuario == "")
+        {
+            $("#mensaje_usuario").fadeIn("");
+            return false;
+        }
+        else
+        {
+            $("#mensaje_usuario").fadeOut();
+
+            if(contraseña == "")
+            {
+                $("#mensaje_password").fadeIn("");
+                return false;
+            }
+            else{
+                $("#mensaje_password").fadeOut();
+            }
+        }
     }
